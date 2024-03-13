9a4ca9c79f638d52467a579cd13f7556aad88df6
Mikolaj <victeron@gmail.com>
2024-03-13 23:43:40 +0100

    users controller

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [

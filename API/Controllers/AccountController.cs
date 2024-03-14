0d37ff5de9b3a7cf64374a26e153b4257de95a9b
Mikolaj <victeron@gmail.com>
2024-03-14 20:47:54 +0100

    generate tokens

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,18 +1,21 @@
 ﻿using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 
+#pragma warning disable CA1862
+
 namespace API.Controllers;
 
-public class AccountController(DataContext _context) : BaseApiController
+public class AccountController(DataContext _context, ITokenService _tokenService) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username))
             return BadRequest("Username is taken");
@@ -29,11 +32,15 @@ public class AccountController(DataContext _context) : BaseApiController
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user),
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
@@ -44,10 +51,14 @@ public class AccountController(DataContext _context) : BaseApiController
 
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-        if (memcmp(computedHash, user.PasswordHash, computedHash.Length) == 0)
-            return Ok(user);
+        if (memcmp(computedHash, user.PasswordHash, computedHash.Length) != 0)
+            return Unauthorized("Invalid password");
 
-        return Unauthorized("Invalid password");
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user),
+        };
 
         [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern int memcmp(byte[] b1, byte[] b2, long count);

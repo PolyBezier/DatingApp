0e5eecdd238e7d99e23da2caa251fded50bbebf6
Mikolaj <victeron@gmail.com>
2024-03-15 01:32:58 +0100

    register ui

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -15,9 +15,20 @@ export class AccountService {
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post<User>(`${this.baseUrl}account/login`, model).pipe(
       map((response: User) => {
         const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(`${this.baseUrl}account/register`, model).pipe(
+      map(user => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);

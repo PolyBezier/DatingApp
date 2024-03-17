2fb0bd96feff0cbc8fc07f505b5934eb15572160
Mikolaj <victeron@gmail.com>
2024-03-17 20:50:32 +0100

    update endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers;
 
@@ -21,5 +22,22 @@ public class UsersController(IUserRepository _userRepository, IMapper _mapper) :
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         return (await _userRepository.GetMemberAsync(username))!;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "";
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null)
+            return NotFound();
+
+        _mapper.Map(memberUpdateDto, user);
+
+        if (await _userRepository.SaveAllAsync())
+            return NoContent();
+
+        return BadRequest("Failed to update user");
     }
 }

e77e6cc70e1aae25012ce048218ce5df08cafd77
Mikolaj <victeron@gmail.com>
2024-03-26 00:59:35 +0100

    messages scroll

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,25 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TimeagoModule } from 'ngx-timeago';
-import { Message } from 'src/app/_models/message';
 import { MessagesService } from 'src/app/_services/messages.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
   imports: [CommonModule, TimeagoModule, FormsModule]
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent implements AfterViewChecked {
   @ViewChild('messageForm') messageForm?: NgForm;
+  @ViewChild('scrollMe') scroll?: ElementRef;
   @Input() username?: string;
   messageContent = '';
 
   constructor(public messageService: MessagesService) { }
 
-  ngOnInit(): void {
+  ngAfterViewChecked(): void {
+    this.scrollDown();
   }
 
   sendMessage() {
@@ -30,5 +32,11 @@ export class MemberMessagesComponent implements OnInit {
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset();
     });
+  }
+
+  scrollDown() {
+    try {
+      this.scroll!.nativeElement.scrollTop = this.scroll?.nativeElement.scrollHeight;
+    } catch { }
   }
 }
